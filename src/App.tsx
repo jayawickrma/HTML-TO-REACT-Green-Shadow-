6014ccf26df5c33a5b5f7030018eb234c0149109
jayawickrama <nishantharaka2004@gmail.com>
2025-01-11 22:37:45 +0530

    created functions of the NavbarPages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import './App.css'
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter, RouterProvider} from "react-router";
 import {RootLayout} from "./Components/ButtonList/RootLayout.tsx";
+import {DashBoard} from "./Pages/DashBoard.tsx";
+import {FieldPage} from "./Pages/Field/FieldPage.tsx";
+import {CropPage} from "./Pages/Crop/CropPage.tsx";
+import {LogPage} from "./Pages/Log/LogPage.tsx";
+import {StaffPage} from "./Pages/Staff/StaffPage.tsx";
+import {VehiclePage} from "./Pages/Vehicle/VehiclePage.tsx";
+import {EquipmentPage} from "./Pages/Equipment/EquipmentPage.tsx";
+
 
 function App() {
 
 const  routes =createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout>,
-            children: [
-
-            ],
-    },
-
-  },
-])
+        {
+            path: '/',
+            element: <RootLayout/>,
+            children:[
+                {path:'',element:<DashBoard/>},
+                {path:'/cropsManagement',element:<CropPage/>},
+                {path:'/fieldManagement',element:<FieldPage/>},
+                {path:'/logManagement',element:<LogPage/>},
+                {path:'/staffManagement',element:<StaffPage/>},
+                {path:'/vehicleManagement',element:<VehiclePage/>},
+                {path:'/equipmentManagement',element:<EquipmentPage/>}
+            ]
+        }
+    ]);
 
   return (
     <>
-
+            <RouterProvider router={routes}>
 
     </>
   )
--- src/Pages/DashBoard.tsx
+++ src/Pages/DashBoard.tsx
@@ -1,4 +1,4 @@
-export function LogPage(){
+export function DashBoard(){
     return(
         <></>
     )
