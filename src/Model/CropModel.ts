0d7751f96b629d093d22659d72ae4ea5baabf52f
jayawickrama <nishantharaka2004@gmail.com>
2025-01-11 22:59:02 +0530

    creating Model Classes

---
+++ src/Model/CropModel.ts
@@ -0,0 +1,23 @@
+export class CropModel{
+
+     cropCode:string;
+     cropName:string;
+     scientificName:string;
+     cropCategory:string;
+     cropSeason:string;
+     cropImage:File|null;
+     logList:string;
+     fieldList:string;
+
+
+    constructor(cropCode: string, cropName: string, scientificName: string, cropCategory: string, cropSeason: string, cropImage: File | null, logList: string, fieldList: string) {
+        this.cropCode = cropCode;
+        this.cropName = cropName;
+        this.scientificName = scientificName;
+        this.cropCategory = cropCategory;
+        this.cropSeason = cropSeason;
+        this.cropImage = cropImage;
+        this.logList = logList;
+        this.fieldList = fieldList;
+    }
+}
---
+++ src/Model/FieldModel.ts
@@ -0,0 +1,24 @@
+export class fieldModel{
+    fieldCode:string;
+    fieldName:string;
+    fieldLocation:string;
+    fieldExtentSize:string;
+    fieldImage1:File|null;
+    fieldImage2:File|null;
+    equipmentList:string;
+    cropList:string;
+    logList:string;
+
+
+    constructor(fieldCode: string, fieldName: string, fieldLocation: string, fieldExtentSize: string, fieldImage1: File | null, fieldImage2: File | null, equipmentList: string, cropList: string, logList: string) {
+        this.fieldCode = fieldCode;
+        this.fieldName = fieldName;
+        this.fieldLocation = fieldLocation;
+        this.fieldExtentSize = fieldExtentSize;
+        this.fieldImage1 = fieldImage1;
+        this.fieldImage2 = fieldImage2;
+        this.equipmentList = equipmentList;
+        this.cropList = cropList;
+        this.logList = logList;
+    }
+}
