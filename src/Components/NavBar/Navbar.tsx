e2ba276e6ab0b160717cf8fe4991f064229e1088
jayawickrama <nishantharaka2004@gmail.com>
2025-01-11 21:11:23 +0530

    eating sidebar button List

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -1,12 +1,19 @@
 import {Link} from "react-router";
 
+
 export function Navbar(){
 return(
     <>
     <header>
-
         <div className="list-group list-group-flush text-center">
-          <Link></Link>
+          <Link to="/">DashBoard</Link>
+            <Link to="">Crop Management</Link>
+            <Link to="">Field Management</Link>
+            <Link to="">Log Management</Link>
+            <Link to="">Equipment Management</Link>
+            <Link to="">Staff Management</Link>
+            <Link to="">Vehicle Management</Link>
+            <Link to="">Settings</Link>
     </div>
     </header>
 
