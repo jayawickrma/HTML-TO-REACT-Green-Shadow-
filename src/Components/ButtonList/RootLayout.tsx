56725330bd8d97eb7c352e49577f6b1c393baa10
jayawickrama <nishantharaka2004@gmail.com>
2025-01-11 23:23:36 +0530

    RootLayout.tsx created

--- src/Components/ButtonList/RootLayout.tsx
+++ src/Components/ButtonList/RootLayout.tsx
@@ -1,9 +1,15 @@
+import {Outlet} from "react-router";
+import {Navbar} from "../NavBar/Navbar.tsx";
+
 export function RootLayout(){
-    return(
+        return(
             <>
-            <div>
-                <section></section>
-            </div>
+                <div className="flex flex-col h-screen bg-gray-800">
+                    <Navbar/>
+                    <section className="absolute right-0 w-[78.5%] h-[97vh] overflow-y-auto p-5">
+                        <Outlet/>
+                    </section>
+                </div>
             </>
-    )
+        )
 }
