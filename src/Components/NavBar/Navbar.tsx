ab2b35adbe36e9b21806f99f5637409d90a8bdac
jayawickrama <nishantharaka2004@gmail.com>
2025-01-12 21:38:29 +0530

    creating Navbar.tsx

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -1,19 +1,58 @@
-    import {Link} from "react-router";
+import { Link } from "react-router";
 
-    export function Navbar(){
-    return(
+export function Navbar() {
+    return (
         <>
-            <div className="list-group list-group-flush text-center">
-                <Link to="/">DashBoard</Link> <br/>
-                <Link to="/crop-management">Crop Management</Link> <br/>
-                <Link to="/field-management">Field Management</Link> <br/>
-                <Link to="/log-management">Log Management</Link> <br/>
-                <Link to="/equipment-management">Equipment Management</Link> <br/>
-                <Link to="/staff-management">Staff Management</Link> <br/>
-                <Link to="/vehicle-management">Vehicle Management</Link> <br/>
-                <Link to="/settings">Settings</Link>
+            <div className="list-group list-group-flush text-center space-y-4 bg-gray-100 py-6 shadow-lg rounded-lg">
+                <Link
+                    to="/"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Dashboard
+                </Link>
+                <Link
+                    to="/crop-management"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Crop Management
+                </Link>
+                <Link
+                    to="/field-management"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Field Management
+                </Link>
+                <Link
+                    to="/log-management"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Log Management
+                </Link>
+                <Link
+                    to="/equipment-management"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Equipment Management
+                </Link>
+                <Link
+                    to="/staff-management"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Staff Management
+                </Link>
+                <Link
+                    to="/vehicle-management"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Vehicle Management
+                </Link>
+                <Link
+                    to="/settings"
+                    className="text-gray-700 font-semibold hover:text-green-500 transition duration-200"
+                >
+                    Settings
+                </Link>
             </div>
-
         </>
-    )
-    }
+    );
+}
