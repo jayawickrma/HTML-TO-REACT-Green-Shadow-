7065a72892753f66e3f1d51b4bcb36580d0a529c
jayawickrama <nishantharaka2004@gmail.com>
2025-01-12 23:18:31 +0530

    created sidebar

--- src/Components/NavBar/SideBar.tsx
+++ src/Components/NavBar/SideBar.tsx
@@ -6,51 +6,50 @@ export function SideBar(){
             <div className="d-flex flex-column justify-content-between bg-dark text-white p-2 vh-100 vw-45">
                 <div>
                 <a className="d-flex align-items-center p-5">
-                    <i className="bi bi-file-arrow-down"></i>
+
                     <span className="fs-4">Green Shadow</span>
                 </a>
                 <hr className="text-secondary mt-2"/>
                 <ul className="nav nav-pills flex-column p-0 m-0">
-                    <li className="nav-item p-1">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-speedometer me-2 fs-5"></i>
-                        <span className="fs-5">Dashboard</span>
+                    <li className="nav-item p-0 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5"> DashBoard</a>
+                        <i className=" me-2 fs-5"></i>
                     </li>
-                    <li className="nav-item p-2">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-speedometer me-2 fs-5"></i>
-                        <span className="fs-5">Crop Management</span>
+                    <li className="nav-item p-0 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5">Crop Management</a>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
                     </li>
-                    <li className="nav-item p-2">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-speedometer me-2 fs-5"></i>
-                        <span className="fs-5">Field Management</span>
+                    <li className="nav-item p-0 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5">Field Management</a>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
                     </li>
-                    <li className="nav-item p-2">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-speedometer me-2 fs-5"></i>
-                        <span className="fs-5">Log Management</span>
+                    <li className="nav-item p-0 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5">Log Management</a>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
                     </li>
-                    <li className="nav-item p-2">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-speedometer me-2 fs-5"></i>
-                        <span className="fs-5">Equipment Management</span>
+                    <li className="nav-item p-0 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5">Equipment Management</a>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
                     </li>
-                    <li className="nav-item p-2">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-usb-c me-2 fs-5"></i>
-                        <span className="fs-5">Staff Management</span>
+                    <li className="nav-item p-0 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5">Staff Management</a>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
                     </li>
-                    <li className="nav-item p-2">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-behance me-2 fs-5"></i>
-                        <span className="fs-5">Vehicle Management</span>
+                    <li className="nav-item p-1 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5">Vehicle Management</a>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
                     </li>
 
-                    <li className="nav-item p-1">
-                        <a href="" className="nav-link text-white"></a>
-                        <i className="bi bi-info-circle me-2 fs-5"></i>
-                        <span className="fs-5">Settings</span>
+                    <li className="nav-item p-1 bg-gradient">
+                        <a href="" className="nav-link text-white fs-5">Settings </a>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
                     </li>
                 </ul>
                 </div>
