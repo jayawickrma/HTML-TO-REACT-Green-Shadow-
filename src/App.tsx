9974243d73d04469589097f052a556299f7bf53f
jayawickrama <nishantharaka2004@gmail.com>
2025-01-12 23:37:02 +0530

    sidebar navigations creating

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {SideBar} from "./Components/NavBar/SideBar.tsx";
+import {BrowserRouter, Routes, Route} from "react-router";
 
 function App() {
     return(
         <>
+            <BrowserRouter>
             <div className="d-flex">
                 <div className="col-auto">
                    <SideBar/>
                 </div>
+                <div>
+                    <Routes>
+                        <Route path="/" element={<h1>Home</h1>} />
+                        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+                        <Route path="/fieldManagement" element={<h1>Field Management</h1>} />
+                        <Route path="/cropManagement" element={<h1>Crop Management</h1>} />
+                        <Route path="/logManagement" element={<h1>Log Management</h1>} />
+                        <Route path="/staffManagement" element={<h1>Staff Management</h1>} />
+                        <Route path="/vehicleManagement" element={<h1>Vehicle Management</h1>} />
+                        <Route path="/equipmentManagement" element={<h1>Equipment Management</h1>} />
+                    </Routes>
+                </div>
             </div>
+            </BrowserRouter>
         </>
     )
 }
--- src/Components/NavBar/SideBar.tsx
+++ src/Components/NavBar/SideBar.tsx
@@ -1,9 +1,10 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import {Link} from "react-router";
 
 export function SideBar(){
     return(
         <>
-            <div className="d-flex flex-column justify-content-between bg-dark text-white p-2 vh-100 vw-45">
+            <div className=" sidebar d-flex flex-column justify-content-between text-white p-2 vh-100 vw-45 bg-dark">
                 <div>
                 <a className="d-flex align-items-center p-5">
 
@@ -12,42 +13,42 @@ export function SideBar(){
                 <hr className="text-secondary mt-2"/>
                 <ul className="nav nav-pills flex-column p-0 m-0">
                     <li className="nav-item p-0 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5"> DashBoard</a>
+                        <Link to="/Home" className="nav-link text-white fs-5 "> DashBoard</Link>
                         <i className=" me-2 fs-5"></i>
                     </li>
                     <li className="nav-item p-0 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5">Crop Management</a>
-                        <i className=" me-2 fs-5"></i>
-                        <span className="fs-5"></span>
-                    </li>
-                    <li className="nav-item p-0 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5">Field Management</a>
+                        <Link to="" className="nav-link text-white fs-5">Crop Management </Link>
                         <i className=" me-2 fs-5"></i>
                         <span className="fs-5"></span>
                     </li>
                     <li className="nav-item p-0 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5">Log Management</a>
+                        <Link to="" className="nav-link text-white fs-5">Field Management</Link>
                         <i className=" me-2 fs-5"></i>
                         <span className="fs-5"></span>
                     </li>
                     <li className="nav-item p-0 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5">Equipment Management</a>
+                        <Link to="" className="nav-link text-white fs-5">Log Management</Link>
                         <i className=" me-2 fs-5"></i>
                         <span className="fs-5"></span>
                     </li>
                     <li className="nav-item p-0 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5">Staff Management</a>
+                        <Link to="" className="nav-link text-white fs-5">Equipment Management</Link>
+                        <i className=" me-2 fs-5"></i>
+                        <span className="fs-5"></span>
+                    </li>
+                    <li className="nav-item p-0 bg-gradient text-center">
+                        <Link to="" className="nav-link text-white fs-5">Staff Management</Link>
                         <i className=" me-2 fs-5"></i>
                         <span className="fs-5"></span>
                     </li>
                     <li className="nav-item p-1 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5">Vehicle Management</a>
+                        <Link to="" className="nav-link text-white fs-5">Vehicle Management</Link>
                         <i className=" me-2 fs-5"></i>
                         <span className="fs-5"></span>
                     </li>
 
                     <li className="nav-item p-1 bg-gradient text-center">
-                        <a href="" className="nav-link text-white fs-5">Settings </a>
+                        <Link to="" className="nav-link text-white fs-5">Settings </Link>
                         <i className=" me-2 fs-5"></i>
                         <span className="fs-5"></span>
                     </li>
