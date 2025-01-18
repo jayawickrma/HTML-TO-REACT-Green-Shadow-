f1c93be1cc5cfb096efcb9fd5a660ff9a7cc67f1
jayawickrama <nishantharaka2004@gmail.com>
2025-01-18 13:38:07 +0530

    created log page

--- src/Components/LogComponent.tsx
+++ src/Components/LogComponent.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ManageLogs from "../Pages/Log/LogPage.tsx";
 
 const LogsComponent: React.FC = () => (
     <div>
-
         <h2>Log Management</h2>
         <button>Add Log</button>
+        <ManageLogs/>
         <table>
             <thead>
             <tr>
